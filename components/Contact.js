71fdfd0d01623ba43e8c9244f4a53c3f87afcf36
Chris <christopher.sarm15@gmail.com>
2023-06-08 17:35:45 -0400

    added get form io but cant get form to clear after submit

--- components/Contact.js
+++ components/Contact.js
@@ -1,6 +1,57 @@
 import React, { useState } from "react";
 
 const Contact = () => {
+  function validateEmail(email) {
+    const regEx =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regEx.test(String(email).toLowerCase());
+  }
+
+  const [formState, setFormState] = useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // destructuring form state
+  const { name, phoneNumber, email, message } = formState;
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+
+      if (!isValid) {
+        setErrorMessage("Your email is invalid!");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required!`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  /* function handleSubmit(e) {
+    setFormState({
+      name: "",
+      phoneNumber: "",
+      email: "",
+      message: "",
+    });
+  } */
+
+
   return (
     <div id="contact" className="w-full lg:h-screen pt-24 pb-10">
       <div className="max-w-[1240px] m-auto px-4 w-full">
@@ -10,7 +61,11 @@ const Contact = () => {
         </p> */}
 
         <div className="w-full h-auto shadow-xl shadow-gray-400 rounded-xl p-4">
-          <form>
+          <form
+          /* onSubmit={handleSubmit} */
+            method="POST"
+            action="https://getform.io/f/1c0b62ce-3b87-4b95-9b8b-388347876616"
+          >
             <div className="grid md:grid-cols-2 gap-4 w-full py-2 ">
               {/* Name */}
               <div className="flex flex-col">
@@ -18,7 +73,9 @@ const Contact = () => {
                 <input
                   className="border-2 rounded-lg p-3"
                   type="text"
+                  name="name"
                   placeholder="Name"
+                  onBlur={handleChange}
                 />
               </div>
 
@@ -27,8 +84,10 @@ const Contact = () => {
                 <label className="text-sm py-2">Phone Number</label>
                 <input
                   className="border-2 rounded-lg p-3"
-                  type="text"
+                  type="number"
+                  name="phoneNumber"
                   placeholder="Phone Number"
+                  onBlur={handleChange}
                 />
               </div>
             </div>
@@ -39,9 +98,13 @@ const Contact = () => {
               <input
                 className="border-2 rounded-lg p-3"
                 type="text"
+                name="email"
                 placeholder="Email"
+                onBlur={handleChange}
               />
             </div>
+            {/* required by getformio so no spam  */}
+            <input type="hidden" name="_gotcha" className="!hidden "></input>
 
             {/* Message */}
             <div className="flex flex-col">
@@ -49,12 +112,15 @@ const Contact = () => {
               <textarea
                 className="border-2 rounded-lg p-3"
                 type="text"
+                name="message"
                 placeholder="Message"
                 rows={10}
+                onBlur={handleChange}
               />
             </div>
 
             <div className="flex flex-col items-center">
+              {errorMessage && <p className="text-red-500">{errorMessage}</p>}
               <button className="w-[200px] md:w-[400px] p-4 mt-4 bg-gradient-to-r from-[#0a2463] to-[#247ba0] text-white rounded-xl shadow-xl shadow-gray-400 ">
                 Sumbit!
               </button>
